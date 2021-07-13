3491f3ca6f967583c290e972144a0e23377f6d7d
unknown <you@example.com>
2021-07-13 16:55:40 -0400

    testing

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -10,6 +10,7 @@ from .views import (
 from . import views
 
 urlpatterns = [
+    
     path('', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
@@ -18,4 +19,5 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
     path('search_users', views.search_users, name='search-users'),
+    path('<int:year>/<str:month>/', views.home, name="home"),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.views.generic import (
@@ -82,11 +85,40 @@ def about(request):
 def search_users(request):
     if request.method == "POST":
         searched = request.POST['searched']
+       
 
         return render(request, 
         'blog/search_users.html',
-        {'searched':searched})
+        {'searched': searched})
     else:
         return render(request,
         'blog/search_users.html',
         {})
+
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+	name = "John"
+	month = month.capitalize()
+	# Convert month from name to number
+	month_number = list(calendar.month_name).index(month)
+	month_number = int(month_number)
+
+	# create a calendar
+	cal = HTMLCalendar().formatmonth(
+		year, 
+		month_number)
+	# Get current year
+	now = datetime.now()
+	current_year = now.year
+	
+	# Get current time
+	time = now.strftime('%I:%M %p')
+	return render(request, 
+		'events/home.html', {
+		"name": name,
+		"year": year,
+		"month": month,
+		"month_number": month_number,
+		"cal": cal,
+		"current_year": current_year,
+		"time":time,
+		})
